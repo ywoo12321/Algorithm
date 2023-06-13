edee2e4da61a10dd0aae5a002f362c772c530501
YeongwooKim <vp4hmzmgyc@privaterelay.appleid.com>
2023-06-13 19:11:50 +0900

    11650 problem

---
+++ 0613/11650.js
@@ -0,0 +1,31 @@
+let fs = require("fs");
+let input = fs.readFileSync("input.txt").toString().split("\n");
+
+const time = Number(input.shift());
+const numberArray = new Array();
+
+for (let i = 0; i < time; i++) {
+  let [x, y] = input[i].split(" ");
+  numberArray.push([x, y]);
+}
+
+numberArray.map((numbers) => {
+  numbers[0] = Number(numbers[0]);
+  numbers[1] = Number(numbers[1].split("\r")[0]);
+});
+
+const compareCoordinate = (a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1];
+  } else if (a[0] !== b[0]) {
+    return a[0] - b[0];
+  }
+};
+
+numberArray.sort(compareCoordinate);
+
+let answer = "";
+for (let i = 0; i < numberArray.length; i++) {
+  answer += numberArray[i][0] + " " + numberArray[i][1] + "\n";
+}
+console.log(answer);
